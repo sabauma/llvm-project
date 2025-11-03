506d7c8b3a9ddbd1de5d4d5efa1f21b43b5ac7ee
Spenser Bauman <sabauma@fastmail>
2025-11-03 16:04:15 -0500

    Disable second

--- llvm/lib/Target/AMDGPU/AMDGPUUniformIntrinsicCombine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUUniformIntrinsicCombine.cpp
@@ -124,7 +124,7 @@ static bool runUniformIntrinsicCombine(Function &F, const UniformityInfo &UI) {
 
     switch (II->getIntrinsicID()) {
     // case Intrinsic::amdgcn_permlane64:
-    case Intrinsic::amdgcn_readfirstlane:
+    // case Intrinsic::amdgcn_readfirstlane:
     case Intrinsic::amdgcn_readlane:
     case Intrinsic::amdgcn_ballot:
       break;
